1dec113e309a648a6b8cf2fbba3a220fc8c0f536
Muntazir Rizvi <smmr143@gmail.com>
2024-06-01 16:19:03 +0500

    signup api along with required packages

--- server/index.js
+++ server/index.js
@@ -1,3 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
+require("dotenv/config");
 
 const app = express();
+const env = process.env;
+const PORT = 3000;
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+
+mongoose
+  .connect(env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {});
---
+++ server/routes/auth.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+const User = require("../models/user");
+
+const authRouter = express.Router();
+
+authRouter.post("/api/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ msg: "User with same email already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 8);
+
+    let user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    user = await user.save();
+    return res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+module.exports = authRouter;
